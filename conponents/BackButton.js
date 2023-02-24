52c9426fb5dac727cc10a2f79bab4762d6edfbe1
boon2187 <fumiya.mai.ito@gmail.com>
2023-02-24 15:33:53 +0900

    戻るボタンをマテリアルアイコンに変更

--- conponents/BackButton.js
+++ conponents/BackButton.js
@@ -1,14 +1,16 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import BackIcon from '@mui/icons-material/ArrowBackIosNew'
+import Button from '@mui/material/Button'
 
 const BackButton = () => {
     const router = useRouter()
 
     return (
         <div>
-            <button alia-label="戻る" type="button" onClick={() => router.back()}>
-                戻る
-            </button>
+            <Button aria-label="戻る" variant="contained" onClick={() => router.back()}>
+                <BackIcon />
+            </Button>
         </div>
     )
 }
