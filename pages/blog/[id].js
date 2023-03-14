d38e3ed173e968dfc8ae22434c700884612add6f
boon2187 <fumiya.mai.ito@gmail.com>
2023-03-14 10:48:08 +0900

    アイキャッチ画像がない場合の処理を追加

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -36,16 +36,21 @@ export default function BlogId ({ blog }) {
 
     return (
         <main className={styles.main}>
-            <Image 
-                src={blog.eyecatchimage.url}
-                width={200}
-                height={100}
-                sizes="100vw"
-                style={{
-                    width: '75%',
-                    height: 'auto',
-                }}
-                alt="ペンギン" />
+            {blog.eyecatchimage ? (
+                <Image 
+                    src={blog.eyecatchimage.url}
+                    width={200}
+                    height={100}
+                    sizes="100vw"
+                    style={{
+                        width: '75%',
+                        height: 'auto',
+                    }}
+                    alt="ペンギン" />
+            ) : (
+                <></>
+            )} 
+
             <h1 className={styles.title}>{blog.title}</h1>
             <p className={styles.publishedAt}>{blog.publishedAt}</p>
             <div dangerouslySetInnerHTML={{__html: `${blog.body}`}} className={styles.post} ></div>
